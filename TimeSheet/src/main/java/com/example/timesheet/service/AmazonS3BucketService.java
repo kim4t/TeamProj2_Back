fcd2b381686787992b6e2c5f46db72eba42acf1c
CENTRAL\kim4t <kim4t@cmich.edu>
2021-11-04 21:47:21 -0400

    S3 server

---
+++ TimeSheet/src/main/java/com/example/timesheet/service/AmazonS3BucketService.java
@@ -0,0 +1,76 @@
+package com.example.timesheet.service;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Service
+public class AmazonS3BucketService {
+    private AmazonS3 amazonS3;
+
+    @Value("${endpointUrl}")
+    private String endpointUrl;
+    @Value("${bucketName}")
+    private String bucketName;
+    @Value("${accessKey}")
+    private String accessKey;
+    @Value("${secretKey}")
+    private String secretKey;
+    @Value("${region}")
+    private String region;
+
+
+    @PostConstruct
+    private void initializeAmazon() {
+        BasicAWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        this.amazonS3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(creds)).withRegion(region).build();
+
+        /*AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        this.amazonS3 = new AmazonS3Client(credentials);
+         */
+    }
+
+    public String uploadFile(MultipartFile multipartFile, String userName) {
+        String fileURL = "";
+        String fileName = "";
+        try {
+            File file = convertMultipartFileToFile(multipartFile);
+            fileName = multipartFile.getOriginalFilename();
+            //TODO: add username to the file
+            fileName = userName + "_" + fileName;
+            fileURL = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileToBucket(fileName, file);
+            file.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(fileURL);
+        //https://proj-angular-bucket.s3.us-east-2.amazonaws.com/testFile.docx
+        return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + fileName;
+    }
+
+    private File convertMultipartFileToFile(MultipartFile file) throws IOException {
+        File convertedFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
+    }
+
+    private void uploadFileToBucket(String fileName, File file) {
+        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+}
